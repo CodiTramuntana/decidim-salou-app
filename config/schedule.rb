8db91ef0e7428c12a2ab48de5e21b9c74c9bc3d5
ailobe <aitorlopezbeltran@gmail.com>
2019-02-20 10:19:13 +0100

    Configure delayed jobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,15 @@
+# frozen_string_literal: true
+
 env :PATH, ENV['PATH']
 
 every :sunday, at: '4:00 am' do
-  rake "decidim:delete_data_portability_files"
+  rake 'decidim:delete_data_portability_files'
+end
+
+every :sunday, at: '5:00 am' do
+  rake 'decidim:open_data:export'
 end
 
 every 1.day, at: '5:00 am' do
-  rake "decidim:metrics:all"
+  rake 'decidim:metrics:all'
 end
